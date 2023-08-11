e2eade5ed492cfe2ec91e92db22ef8e964cb1717
kipgeoffry <kipgeoffry@gmail.com>
2023-08-11 23:59:45 +0300

    added controller wrapper to handle try/catch exceptions

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,6 +1,7 @@
 const Book = require('../models/schemas/books');
 const ApiError = require('../utils/ApiError')
 const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
 
 //@desc adding a book handler
 //@route POST /api/books/add
@@ -34,64 +35,48 @@ const getAllBooks = async (req,res,next)=>{
 //@desc get books by author
 //@route GET /api/books/search
 //@access private/protected
-const getBooks = async(req, res, next)=>{
+const getBooks = catchAsync(async(req, res, next)=>{
     const { author } = req.query;
-    try {
-        const book = await Book.findOne({author});
-        // if(!book) return res.status(404).json({message:"Book not found"})
-        if(!book) throw new ApiError(httpStatus.NOT_FOUND, `No books by author: ${author} found`);
-        res.status(200).send(book);
-    } catch (error) {
-        next(error);
-    }
-};
+    const book = await Book.findOne({author});
+    // if(!book) return res.status(404).json({message:"Book not found"})
+    if(!book) throw new ApiError(httpStatus.NOT_FOUND, `No books by author: ${author} found`);
+    res.status(200).send(book);
+    });
 
 
 //@desc get a book by id Handler
 //@route GET /api/books/:id
 //@access private/protected
-const getBook = async (req,res,next)=>{
+const getBook = catchAsync(async (req,res,next)=>{
     const { id } = req.params
-    try {
-        const getBook = await Book.findById(id)
-        if(!getBook) return res.status(404).json({"message":`Book with id ${id} not found`})
-        res.status(200).send(getBook)
-    } catch (error) {
-        // res.status(400).json({"error":error.name,"message":error.message})
-        next(error)
-    }
-};
+    const getBook = await Book.findById(id)
+    // if(!getBook) return res.status(404).json({"message":`Book with id ${id} not found`})
+    if(!getBook) throw new ApiError(httpStatus.NOT_FOUND, `Book with id ${id} not found`)
+    res.status(200).send(getBook)
+});
 
 //@desc update book handler
 //@route PATCH /api/books/update/:id
 //@access private/protected
-const updateBook = async (req,res) => {
+const updateBook = catchAsync(async (req,res) => {
     const { id } = req.params;
     const book = req.body;
-    try {
-        const updateBook = await Book.findOneAndUpdate({_id: id}, book, { new: true }) //(filter,update,options)
-        res.status(200).json({"message":"Book updated"})
-    } catch (error) {
-        // res.status(400).json({"error":error.name,"message":error.message})
-        next(error)
-    }
-};
+    const updateBook = await Book.findOneAndUpdate({_id: id}, book, { new: true }) //(filter,update,options)
+    if(!updateBook) throw new ApiError(httpStatus.NOT_FOUND, "Book not found");
+    res.status(200).json({"message":"Book updated successfully"})
+});
 
 
 //@desc delete a book handler
 //@route DELETE /api/books/delete/:id
 //@access private/protected
-const deleteBook = async (req,res,next)=>{
+const deleteBook = catchAsync(async (req,res,next)=>{
     const { id } = req.params
-    try {
-        const getBook = await Book.findByIdAndDelete(id)
-        if(!getBook) return res.status(404).json({"message":`Book not found`})
-        res.status(200).json({"message":"Book deleted sucessfully"})
-    } catch (error) {
-        // res.status(400).json({"error":error.name,"message":error.message})
-        next(error)
-    }
-};
+    const getBook = await Book.findByIdAndDelete(id)
+    // if(!getBook) return res.status(404).json({"message":`Book not found`})
+    if(!getBook) throw new ApiError(httpStatus.NOT_FOUND, `Book not found`)
+    res.status(200).json({"message":"Book deleted sucessfully"})
+});
 
 
 
