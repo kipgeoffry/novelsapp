20e02ae3ac431a6d74f8be311f73dc77dac16790
kipgeoffry <kipgeoffry@gmail.com>
2023-08-22 22:35:58 +0300

    refactoring code by adding src folder

--- utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-//handler that will handle the try/catch errors.
-//wrap this in a function so that it can catch exceptions
-
-const catchAsync = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next))
-           .catch((err) => next(err));
-};
-
-module.exports = catchAsync;
